2be0c8631994f62e5c5f347e61cd9f014ebbc829
pvalle6 <pvalle6@lsu.edu>
2022-09-27 14:04:28 -0500

    Memory Node Issue Fixed

--- Code/Python_Lines_Glycowork_HPC.py
+++ Code/Python_Lines_Glycowork_HPC.py
@@ -22,48 +22,61 @@ model_esm, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 
 ##### PROGRAM #####
 
+maxRow = 10000
+rowSkip = 0
+nrowsCount = 2000
 filepath = "/home/pvalle6/Chimeras.output"	
-file_input = pd.read_csv(filepath, header = None)
-file_input.columns = ['input']
-file_input = file_input.input.str.split(expand = True)
-proteinNameSeq = file_input.drop([1,2], axis = 1)
-proteinNameSeq.columns = ['NCR', 'SEQUENCE']
-protein_embeddings = []
-
-protein_seq = proteinNameSeq['SEQUENCE'].tolist()
-protein_dict = get_esm1b_representations(protein_seq, model_esm, alphabet)
-
-model = prep_model('LectinOracle',1,trained=True)
-outprint_multi_protein = pd.DataFrame(columns = ['name', 'preds'])
-glycan = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
-
-#multiple protein, single glycan prediction getter
-a=0
-for index, rows in proteinNameSeq.iterrows():
-    outprint_multi_protein.at[a, 'name'] = rows['NCR'] # replace with the actual name when reading from a file
-    #[pred] is at the end because get_lec returns an array and we only need the pred value
-    outprint_multi_protein.at[a, 'preds'] = str((get_lectin_preds((rows['SEQUENCE']), glycan,model,protein_dict))['pred'])
-    a +=1
-
-nameDF = outprint_multi_protein['name']
-splitDF = outprint_multi_protein.preds.str.split(expand = True)
-splitDF = splitDF.drop([0,2,3,4,5], axis = 1)
-concatDF = pd.concat([nameDF, splitDF], ignore_index=True, sort=False, axis = 1)
-
-sortedConcatDF = concatDF.sort_values(concatDF.columns[1])
-rangeDF = concatDF[concatDF.columns[1]]
-
-maximum = rangeDF.max(axis=0)
-minimum = rangeDF.min(axis=0)
-
-text_file = open("/ddnA/project/jjung1/pvalle6/range.txt", "w")
-text_file.write('max:' + maximum)
-text_file.write('\n')
-text_file.write('min:' + minimum)
-text_file.close()
-
-#concatDF.to_csv("/ddnA/project/jjung1/pvalle6/output_original.csv", index = False)
-sortedConcatDF.to_csv("/ddnA/project/jjung1/pvalle6/output_sorted.csv",header=False, index = False)
 
 
-####
+#possible make this a function
+while((rowSkip - nrowsCount) < maxRow):
+    file_input = pd.read_csv(filepath, skiprows = rowSkip, nrows = nrowsCount, header = None)
+    #iterates the while loop
+    rowSkip = rowSkip + nrowsCount
+
+    file_input.columns = ['input']
+    file_input = file_input.input.str.split(expand = True)
+    proteinNameSeq = file_input.drop([1,2], axis = 1)
+    proteinNameSeq.columns = ['NCR', 'SEQUENCE']
+    protein_embeddings = []
+
+    protein_seq = proteinNameSeq['SEQUENCE'].tolist()
+    protein_dict = get_esm1b_representations(protein_seq, model_esm, alphabet)
+
+    model = prep_model('LectinOracle',1,trained=True)
+    outprint_multi_protein = pd.DataFrame(columns = ['name', 'preds'])
+    glycan = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
+
+    #multiple protein, single glycan prediction getter
+    a=0
+    for index, rows in proteinNameSeq.iterrows():
+        outprint_multi_protein.at[a, 'name'] = rows['NCR'] # replace with the actual name when reading from a file
+        #[pred] is at the end because get_lec returns an array and we only need the pred value
+        outprint_multi_protein.at[a, 'preds'] = str((get_lectin_preds((rows['SEQUENCE']), glycan,model,protein_dict))['pred'])
+        a +=1
+
+    nameDF = outprint_multi_protein['name']
+    splitDF = outprint_multi_protein.preds.str.split(expand = True)
+    splitDF = splitDF.drop([0,2,3,4,5], axis = 1)
+    concatDF = pd.concat([nameDF, splitDF], ignore_index=True, sort=False, axis = 1)
+
+    sortedConcatDF = concatDF.sort_values(concatDF.columns[1])
+    rangeDF = concatDF[concatDF.columns[1]]
+
+    maximum = rangeDF.max(axis=0)
+    minimum = rangeDF.min(axis=0)
+
+    outRange = (f"/ddnA/project/jjung1/pvalle6/preds/range/range.txt")
+    text_file = open(outRange, "a")
+    text_file.write(maximum)
+    text_file.write('\n')
+    text_file.write(minimum)
+    text_file.close()
+
+    
+    
+    outPreds = (f"/ddnA/project/jjung1/pvalle6/preds/sorted/output_sorted.csv")
+    sortedConcatDF.to_csv(outPreds,mode = 'a',header=False, index = False)
+
+
+    ####
