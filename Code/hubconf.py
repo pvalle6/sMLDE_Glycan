f5bdfa5fd038b25c3539f60705bf634f0ef7486a
pvalle6 <pvalle6@lsu.edu>
2022-09-27 21:32:56 -0500

    moved scripts into dedicated folder

--- Code/hubconf.py
+++
@@ -1,9 +0,0 @@
-dependencies = ['torch']
-import esm  
-
-# resnet18 is the name of entrypoint
-def entry():
-    checkpoint = "/ddnA/project/jjung1/pvalle6/checkpoints/esm1b_t33_650M_UR50S.pt"
-    state_dict = torch.load(checkpoint)
-    model.load_state_dict(state_dict)
-    return model
