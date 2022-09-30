e1a22874f7f76ba46190d4e392abc919bc22de65
pvalle6 <pvalle6@lsu.edu>
2022-09-30 12:25:21 -0500

    added statistics folder and started statistics scripts

--- Code/Script_Sub_combos/Statistics/range.py
+++ Code/Script_Sub_combos/Statistics/range.py
@@ -1,7 +1,7 @@
 import pandas
 
 fileIn = "/ddnA/project/jjung1/pvalle6/preds/sorted/output_sorted.csv"
-df = pd.read_csv(fileIn ,header = None,index_col=False)
+df = pd.read_csv(fileIn,header = None,index_col=False)
 minVal = df.min()
 maxVal = df.max()
 
