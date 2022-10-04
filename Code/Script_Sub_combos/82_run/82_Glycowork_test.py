65b0dfcbb20067178d7cf207acf9469ad2e8878f
pvalle6 <pvalle6@lsu.edu>
2022-10-04 17:54:41 -0500

    fixed path

--- Code/Script_Sub_combos/82_run/82_Glycowork_test.py
+++ Code/Script_Sub_combos/82_run/82_Glycowork_test.py
@@ -19,31 +19,17 @@ model_esm, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 #model, alphabet = torch.hub.load("/ddnA/project/jjung1/pvalle6/checkpoints", "esm1b_t33_650M_UR50S", source ='local')
 
 ##### PROGRAM #####
-
-#length of the chimera file rows
-maxRow = 82
-#initial row skip is 0 due to starting at 1
-#adjust for continuation of previous job
-rowSkip = 0
-# memory excedes past 3000 per run
-nrowsCount = 82
 filepath = "/ddnA/project/jjung1/pvalle6/82_high.csv"	
 
 #possibly make this a function
 #if continuing previous job, adjust row skip 
 #split into 2000 iterations to allow garbage collection 
 while((rowSkip - nrowsCount) < maxRow):
-    file_input = pd.read_csv(filepath, skiprows = rowSkip, nrows = nrowsCount, header = None)
-    #iterates the while loop
-    rowSkip = rowSkip + nrowsCount
-
-    #file_input.columns = ['input']
-    #file_input = file_input.input.str.split(expand = True)
-    #proteinNameSeq = file_input.drop([1,2], axis = 1)
+    file_input = pd.read_csv(filepath,, header = None)
     file_input.columns = ['UNI', 'SEQUENCE']
     protein_embeddings = []
 
-    protein_seq = proteinNameSeq['SEQUENCE'].tolist()
+    protein_seq = file_input['SEQUENCE'].tolist()
     protein_dict = get_esm1b_representations(protein_seq, model_esm, alphabet)
 
     model = prep_model('LectinOracle',1,trained=True)
@@ -56,7 +42,7 @@ while((rowSkip - nrowsCount) < maxRow):
     #multiple protein, single glycan prediction getter
     # calls the list of proteins with their embedding pairs and a selected glycan
     a=0
-    for index, rows in proteinNameSeq.iterrows():
+    for index, rows in file_input.iterrows():
         outprint_multi_protein.at[a, 'name'] = rows['UNI'] # replace with the actual name when reading from a file
         #[pred] is at the end because get_lec returns an array and we only need the pred value
         outprint_multi_protein.at[a, 'preds'] = str((get_lectin_preds((rows['SEQUENCE']), glycan,model,protein_dict))['pred'])
--- Code/Script_Sub_combos/Statistics/histogram.py
+++ Code/Script_Sub_combos/Statistics/histogram.py
@@ -1,8 +1,8 @@
 import pandas
 import matplotlib.pyplot as plt
-fileIn = R"C:\Users\valle\Downloads\output_sorted.csv"
+fileIn = R"C:\Users\valle\OneDrive\Documents\EP_DE_Aggrin\Statistics\82_out.csv"
 #fileIn = "/ddnA/project/jjung1/pvalle6/preds/sorted/output_sorted.csv"
 df = pandas.read_csv(fileIn,header = None,index_col=False)
 
-plot = df.plot.hist(column=1,range=[0.1, 1.2],edgecolor='black', bins = 1000)
-plt.savefig('hist.pdf')
+plot = df.plot.hist(column=1,range=[0.1, 1.2],edgecolor='black', bins = 100)
+plt.savefig(R"C:\Users\valle\OneDrive\Documents\EP_DE_Aggrin\Statistics\82_hist.pdf")
