d5a134d69d7e58a4ab54712d4bb0d01e092f5172
Peter <pvalle6@lsu.edu>
2023-01-14 14:46:39 -0600

    Multi works

--- Code/Script_Sub_combos/Combined_NON_NCR_Glycowork/Combined_NON_NCR.py
+++ Code/Script_Sub_combos/Combined_NON_NCR_Glycowork/Combined_NON_NCR.py
@@ -13,17 +13,20 @@ from glycowork.glycan_data.data_entry import *
 import esm
 
 torch.hub.set_dir('/ddnA/project/jjung1/pvalle6/')
-#it appears that this function checks for local aswell, don't believe its redownloading 
+# it appears that this function checks for local aswell, don't believe its redownloading
 model_esm, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
-#pure local model call
-#model, alphabet = torch.hub.load("/ddnA/project/jjung1/pvalle6/checkpoints", "esm1b_t33_650M_UR50S", source ='local')
+# pure local model call
+# model, alphabet = torch.hub.load("/ddnA/project/jjung1/pvalle6/checkpoints", "esm1b_t33_650M_UR50S", source ='local')
 
 ##### PROGRAM #####
 dimer = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
 tetramer = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
-decamer = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
-twentymer = ['GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl'] 
-lectin = "/ddnA/project/jjung1/pvalle6/82.csv" 
+decamer = [
+    'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
+twentymer = [
+    'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl']
+lectin = "/ddnA/project/jjung1/pvalle6/82.csv"
+
 
 def run_glycowork(lectin_doc, glycanIN):
     glycan = glycanIN
@@ -31,36 +34,37 @@ def run_glycowork(lectin_doc, glycanIN):
     rowSkip = 0
     maxRow = 82
     nrowsCount = 82
-    #possibly make this a function
-    #if continuing previous job, adjust row skip 
-    #split into 2000 iterations to allow garbage collection 
-    while((rowSkip) < maxRow):
-        file_input = pd.read_csv(filepath, header = None)
+    # possibly make this a function
+    # if continuing previous job, adjust row skip
+    # split into 2000 iterations to allow garbage collection
+    while ((rowSkip) < maxRow):
+        file_input = pd.read_csv(filepath, header=None)
         file_input.columns = ['UNI', 'SEQUENCE']
         rowSkip = rowSkip + nrowsCount
-        #applied fix from 971afa8
+        # applied fix from 971afa8
         protein_seq = file_input['SEQUENCE'].tolist()
         protein_dict = get_esm1b_representations(protein_seq, model_esm, alphabet)
 
-        model = prep_model('LectinOracle',1,trained=True)
-        outprint_multi_protein = pd.DataFrame(columns = ['name', 'preds'])
+        model = prep_model('LectinOracle', 1, trained=True)
+        outprint_multi_protein = pd.DataFrame(columns=['name', 'preds'])
         # original glycan
-        #multiple protein, single glycan prediction getter
+        # multiple protein, single glycan prediction getter
         # calls the list of proteins with their embedding pairs and a selected glycan
-        a=0
+        a = 0
         for index, rows in file_input.iterrows():
-            outprint_multi_protein.at[a, 'name'] = rows['UNI'] # replace with the actual name when reading from a file
-            #[pred] is at the end because get_lec returns an array and we only need the pred value
-            outprint_multi_protein.at[a, 'preds'] = str((get_lectin_preds((rows['SEQUENCE']), glycan,model,protein_dict))['pred'])
-            a +=1
+            outprint_multi_protein.at[a, 'name'] = rows['UNI']  # replace with the actual name when reading from a file
+            # [pred] is at the end because get_lec returns an array and we only need the pred value
+            outprint_multi_protein.at[a, 'preds'] = str(
+                (get_lectin_preds((rows['SEQUENCE']), glycan, model, protein_dict))['pred'])
+            a += 1
 
         # recombines individual predictions and removes unnecessary formatting s
         nameDF = outprint_multi_protein['name']
-        splitDF = outprint_multi_protein.preds.str.split(expand = True)
-        splitDF = splitDF.drop([0,2,3,4,5], axis = 1)
-        concatDF = pd.concat([nameDF, splitDF], ignore_index=True, sort=False, axis = 1)
+        splitDF = outprint_multi_protein.preds.str.split(expand=True)
+        splitDF = splitDF.drop([0, 2, 3, 4, 5], axis=1)
+        concatDF = pd.concat([nameDF, splitDF], ignore_index=True, sort=False, axis=1)
 
-        #sortedConcatDF = concatDF.sort_values(concatDF.columns[1])
+        # sortedConcatDF = concatDF.sort_values(concatDF.columns[1])
         # prediction file appended below
         if glycan == dimer:
             outPreds = (f"/ddnA/project/jjung1/pvalle6/preds/dimer_mammalian_preds.csv")
@@ -70,8 +74,9 @@ def run_glycowork(lectin_doc, glycanIN):
             outPreds = (f"/ddnA/project/jjung1/pvalle6/preds/decamer_mammalian_preds.csv")
         if glycan == twentymer:
             outPreds = (f"/ddnA/project/jjung1/pvalle6/preds/twentymer_mammalian_preds.csv")
-        concatDF.to_csv(outPreds,mode = 'a',header=False, index = False)
+        concatDF.to_csv(outPreds, mode='a', header=False, index=False)
     ####
+
 
 run_glycowork(lectin, dimer)
 run_glycowork(lectin, tetramer)
--- Code/Script_Sub_combos/MultiGlycan_MultiLectin/test_import.py
+++ Code/Script_Sub_combos/MultiGlycan_MultiLectin/test_import.py
@@ -1,5 +1,12 @@
-import pandas as pd
+#import pandas as pd
 filepath = R"C:\Users\Hotsauce141\Downloads\import.txt"
-glycan_file = pd.read_csv(filepath, header = None)
-glycan_list = glycan_file.values.tolist()
-print(glycan_list)
+#glycan_file = pd.read_csv(filepath, header = None)
+#glycan_list = glycan_file.values
+#print(glycan_list)
+
+from numpy import loadtxt
+your_list = []
+lines = loadtxt(filepath, dtype=str, comments="#", delimiter=",", unpack=False)
+
+
+print(lines)
