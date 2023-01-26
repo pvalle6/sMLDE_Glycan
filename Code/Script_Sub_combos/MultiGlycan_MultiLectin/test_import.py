ef66a58517c2262cfed0f49ed6deb3e162673e4e
pvalle6 <pvalle6@lsu.edu>
2023-01-26 09:57:23 -0600

    fixed problem

--- Code/Script_Sub_combos/MultiGlycan_MultiLectin/test_import.py
+++ Code/Script_Sub_combos/MultiGlycan_MultiLectin/test_import.py
@@ -1,12 +1,8 @@
-#import pandas as pd
-filepath = R"C:\Users\Hotsauce141\Downloads\import.txt"
-#glycan_file = pd.read_csv(filepath, header = None)
-#glycan_list = glycan_file.values
-#print(glycan_list)
-
 from numpy import loadtxt
-your_list = []
-lines = loadtxt(filepath, dtype=str, comments="#", delimiter=",", unpack=False)
 
+filepath = R"C:\Users\valle\Downloads\import.csv"
+lines = loadtxt(filepath, dtype=str)
 
-print(lines)
+for i in lines:
+	
+	print(i)
