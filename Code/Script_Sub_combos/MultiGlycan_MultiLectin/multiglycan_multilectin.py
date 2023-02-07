f0601f5cd9aa6c8b7272a59adfe34aba83c299c8
pvalle6 <pvalle6@lsu.edu>
2023-02-07 09:28:28 -0600

    updating other glycowork runs

--- Code/Script_Sub_combos/MultiGlycan_MultiLectin/multiglycan_multilectin.py
+++ Code/Script_Sub_combos/MultiGlycan_MultiLectin/multiglycan_multilectin.py
@@ -24,6 +24,10 @@ LG1 = 'KVAVSADRDCIRAYQPQISSTNYNTLTLNVKTQEPDNLLFYLGSSTASDFLAVEMRRGRVAFLWDLGSGSTRL
 LG2 = 'DPSFHFDGSGYSVVEKSLPATVTQIIMLFNTFSPNGLLLYLGSYGTKDFLSIELFRGRVKVMTDLGSGPITLLTDRRYNNGTWYKIAFQRNRKQGVLAVIDAYNTSNKETKQGETPGASSDLNRLDKDPIYVGGLPRSRVVRRGVTTKSFVGCIKNLEISRSTFDLLRNSYGVRKGC'
 LG3 = 'IRSVSFLKGGYIELPPKSLSPESEWLVTFATTNSSGIILAALGGDVEKRGDREEAHVPFFSVMLIGGNIEVHVNPGDGTGLRKALLHAPTGTCSDGQAHSISLVRNRRIITVQLDENNPVEMKLGTLVESRTINVSNLYVGGIPEGEGTSLLTMRRSFHGCIKNLIFNLELLDFNSAVGHEQVDLDTC'
 
+LG4 = 'DTAPGYVAGAHQFGLSQNSHLVLPLNQSDVRKRLQVQLSIRTFASSGLIYYVAHQNQMDYATLQLQEGRLHFMFDLGKGRTKVSHPALLSDGKWHTVKTEYIKRKAFMTVDGQESPSVTVVGNATTLDVERKLYLGGLPSHYRARNIGTITHSIPACIGEIMVNGQQLDKDRP'
+EFG3= 'LSASA'
+LG5 = 'VDRCYVVAQEGTFFEGSGYAALVKEGYKVRLDLNITLEFRTTSKNGVLLGISSAKVDAIGLEIVDGKVLFHVNNGAGRITATYQPRAARALCDGKWHTLQAHKSKHRIVLTVDGNSVRAESPHTHSTSADTNDPIYVGGYPAHIKQNCLSSRASFRGCVRNLRLSRGSQVQSLDLSR'
+
 EGF1 = 'IEREGKC'
 EGF2 = 'NSYGVRK'
 
@@ -32,25 +36,28 @@ LG2_EG2 = LG2+EGF2
 
 LNA1 = LG1+EGF1+LG2+EGF2+LG3
 
-prot_seq = [LG1,LG2,LG3, LG1_EG1, LG2_EG2, LNA1]
+prot_seq = [LG4, LG5, LG4+EGF3+LG5]
+
+expansion = '(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man'
+repeat = 'GlcA(b1-3)Xyl(a1-3)'
+base = 'GlcA(b1-4)Xyl'
+units_add = ''
+glycans = []
+for i in range(10):
+	units_add = ''
+	for y in range(i):
+		units_add = units_add + repeat
+	glycans.append(units_add + base + expansion)
+
 count=0
 for proteins in prot_seq:
-	lines = ['GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
-	'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl'
-	]
+	
 	lines = ['GlcA(b1-4)Xyl', 'GlcA(b1-4)Xyl']
 
 	protein_emb_dic = get_esm1b_representations([proteins], model_esm, alphabet)
 
 	leor = prep_model('LectinOracle', 1, trained=True)
-	preds = get_lectin_preds(proteins, lines, leor, protein_emb_dic)
+	preds = get_lectin_preds(proteins, glycans, leor, protein_emb_dic)
 	count = count +1
 	preds.to_csv('/ddnA/project/jjung1/pvalle6/preds{0}.csv'.format(count), mode='a', header=False, index=False)
 	#
