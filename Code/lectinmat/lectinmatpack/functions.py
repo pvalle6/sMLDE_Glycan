7e5d205a47cbe61280e6feaf02921fef72e62425
Peter <pvalle6@lsu.edu>
2023-02-25 19:14:13 -0600

    Update functions.py

--- Code/lectinmat/lectinmatpack/functions.py
+++ Code/lectinmat/lectinmatpack/functions.py
@@ -3,11 +3,10 @@
 
 def generate_matriglycan(length: int, extension: bool) -> list:
     """Generates a matriglycan IUPAC-condensed from length"""
-
-    if (extension)
-        expansion = '(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man'
-        repeat = 'GlcA(b1-3)Xyl(a1-3)'
-        base = 'GlcA(b1-4)Xyl'
+    expansion = '(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man'
+    repeat = 'GlcA(b1-3)Xyl(a1-3)'
+    base = 'GlcA(b1-4)Xyl'
+    if extension:
         units_add = ''
         glycans = []
         # count = 0
@@ -18,10 +17,7 @@ def generate_matriglycan(length: int, extension: bool) -> list:
             glycans.append(units_add + base + expansion)
         return glycans
 
-    else
-        expansion = '(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man'
-        repeat = 'GlcA(b1-3)Xyl(a1-3)'
-        base = 'GlcA(b1-4)Xyl'
+    else:
         units_add = ''
         glycans = []
         # count = 0
@@ -29,5 +25,5 @@ def generate_matriglycan(length: int, extension: bool) -> list:
             units_add = ''
             for y in range(i):
                 units_add = units_add + repeat
-            glycans.append(units_add + base + expansion)
+            glycans.append(units_add + base)
         return glycans
