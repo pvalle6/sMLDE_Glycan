6dc40eec390deaab0f895534f2fcc15097795aa3
Peter <pvalle6@lsu.edu>
2023-02-25 19:20:31 -0600

    updated comment for function

--- Code/lectinmat/lectinmatpack/functions.py
+++ Code/lectinmat/lectinmatpack/functions.py
@@ -2,14 +2,15 @@
 
 
 def generate_matriglycan(length: int, extension: bool) -> list:
-    """Generates a matriglycan IUPAC-condensed from length"""
+    """Generates a matriglycan IUPAC-condensed from length
+    length: integer representing number of repeats
+    extension: boolean representing the presence of preceding saccharides"""
     expansion = '(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man'
     repeat = 'GlcA(b1-3)Xyl(a1-3)'
     base = 'GlcA(b1-4)Xyl'
     if extension:
         units_add = ''
         glycans = []
-        # count = 0
         for i in range(length):
             units_add = ''
             for y in range(i):
@@ -20,7 +21,6 @@ def generate_matriglycan(length: int, extension: bool) -> list:
     else:
         units_add = ''
         glycans = []
-        # count = 0
         for i in range(length):
             units_add = ''
             for y in range(i):
