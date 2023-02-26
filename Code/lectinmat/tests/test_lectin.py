51615632826a87682cd221ff49b2421233847fcb
Peter <pvalle6@lsu.edu>
2023-02-25 19:20:19 -0600

    added test for new extension parameter

--- Code/lectinmat/tests/test_lectin.py
+++ Code/lectinmat/tests/test_lectin.py
@@ -1,9 +1,15 @@
 from lectinmatpack.functions import generate_matriglycan
 
 
-def test_func():
-    assert generate_matriglycan(5) == ['GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man',
+def test_pos():
+    assert generate_matriglycan(5,True) == ['GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man',
                                        'GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man',
                                        'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man',
                                        'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man',
                                        'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl(b1-4)RibOP-ol(5-1)RibOP-ol(5-3)GalNAc(b1-3)GlcNAc(b1-4)Man']
+def test_neg():
+    assert generate_matriglycan(5,False) == ['GlcA(b1-4)Xyl',
+                                       'GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
+                                       'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
+                                       'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl',
+                                       'GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-3)Xyl(a1-3)GlcA(b1-4)Xyl']
